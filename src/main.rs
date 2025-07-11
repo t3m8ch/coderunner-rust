f2e6c8818246a48b6bd854a8fd1d334c88944ce4
Artem Kudyakov <t3m8ch@yandex.ru>
2025-07-12 03:52:10 +0400

    feat: Make response to client

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::path::Path;
+use std::sync::Arc;
 
+use dashmap::DashMap;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio::process::Command;
+use tokio::sync::Mutex;
 use tokio::sync::mpsc::channel;
 
-use crate::models::{FileToCompile, FileToRun};
+use crate::models::{FileToCompile, FileToRun, MsgToHandle, MsgToRes};
 
 mod models;
 
@@ -14,66 +17,106 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let listener = TcpListener::bind("0.0.0.0:8000").await?;
     println!("Server listening on port 8000");
 
+    let (read_tx, mut read_rx) = channel::<Arc<Mutex<TcpStream>>>(10);
+    let (msg_handle_tx, mut msg_handle_rx) = channel::<MsgToHandle>(10);
+    let (res_tx, mut res_rx) = channel::<MsgToRes>(10);
     let (compile_tx, mut compile_rx) = channel::<FileToCompile>(10);
     let (run_tx, mut run_rx) = channel::<FileToRun>(10);
 
-    let req_handler = tokio::spawn(async move {
+    let sockets: Arc<DashMap<uuid::Uuid, Arc<Mutex<TcpStream>>>> = Arc::new(DashMap::new());
+
+    let accept_task = tokio::spawn(async move {
         loop {
-            let (mut socket, _) = listener.accept().await.unwrap();
-            let compile_tx = compile_tx.clone();
+            let (socket, _) = listener.accept().await.unwrap();
+            read_tx.send(Arc::new(Mutex::new(socket))).await.unwrap();
+        }
+    });
 
+    let read_task_sockets = Arc::clone(&sockets);
+    tokio::spawn(async move {
+        while let Some(socket) = read_rx.recv().await {
+            let task_id = uuid::Uuid::new_v4();
+            read_task_sockets.insert(task_id, socket.clone());
+
+            let msg_handle_tx = msg_handle_tx.clone();
             tokio::spawn(async move {
-                let mut read_buf = [0u8; 1024];
-
                 loop {
-                    let n = socket.read(&mut read_buf).await.unwrap();
+                    let mut buf = [0u8; 1024];
+
+                    let n = socket.lock().await.read(&mut buf).await.unwrap();
                     if n == 0 {
                         break;
                     }
-                    let msg = String::from_utf8_lossy(&read_buf[0..n]);
 
-                    println!("Received message: {}", msg.trim());
-
-                    let mut msg = msg.trim().split(' ');
-                    match msg.next() {
-                        Some(cmd) => match cmd {
-                            "SUBMIT" => {
-                                let file_name = msg.next();
-                                match file_name {
-                                    Some(file_name) => {
-                                        let file_name = file_name.trim();
-
-                                        compile_tx
-                                            .send(FileToCompile::new(file_name))
-                                            .await
-                                            .unwrap();
-
-                                        let res_msg = format!("File '{}' submitted\n", file_name);
-                                        socket.write_all(res_msg.as_bytes()).await.unwrap();
-                                    }
-                                    None => {
-                                        socket.write_all(b"File name required\n").await.unwrap();
-                                    }
-                                }
-                            }
-                            _ => {
-                                socket.write_all(b"Invalid command\n").await.unwrap();
-                            }
-                        },
-                        None => {
-                            socket.write_all(b"Invalid request\n").await.unwrap();
-                        }
-                    };
+                    msg_handle_tx
+                        .send(MsgToHandle::new(
+                            task_id,
+                            &String::from_utf8_lossy(&buf[0..n]).to_string(),
+                        ))
+                        .await
+                        .unwrap();
                 }
             });
         }
     });
 
+    let msg_handle_res_tx = res_tx.clone();
+    tokio::spawn(async move {
+        let res_tx = msg_handle_res_tx;
+        while let Some(msg) = msg_handle_rx.recv().await {
+            let mut msg_text = msg.text.split(' ');
+
+            let Some(cmd) = msg_text.next() else {
+                res_tx
+                    .send(MsgToRes::new(msg.id, "Invalid request"))
+                    .await
+                    .unwrap();
+                continue;
+            };
+
+            match cmd {
+                "SUBMIT" => {
+                    let Some(file_name) = msg_text.next() else {
+                        res_tx
+                            .send(MsgToRes::new(msg.id, "File name required"))
+                            .await
+                            .unwrap();
+                        continue;
+                    };
+                    let file_name = file_name.trim();
+
+                    compile_tx
+                        .send(FileToCompile::new(msg.id, file_name))
+                        .await
+                        .unwrap();
+
+                    res_tx
+                        .send(MsgToRes::new(
+                            msg.id,
+                            &format!("File {} submitted", file_name),
+                        ))
+                        .await
+                        .unwrap();
+                }
+                _ => {
+                    res_tx
+                        .send(MsgToRes::new(msg.id, "Invalid command"))
+                        .await
+                        .unwrap();
+                }
+            }
+        }
+    });
+
+    let compile_res_tx = res_tx.clone();
     tokio::spawn(async move {
         while let Some(task) = compile_rx.recv().await {
+            let task_res_tx = compile_res_tx.clone();
             let run_tx = run_tx.clone();
 
             tokio::spawn(async move {
+                let res_tx = task_res_tx;
+
                 println!("Compiling file: {:?}", task);
 
                 let file_name = Path::new(&task.file_name);
@@ -88,40 +131,83 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .await
                     .unwrap();
 
-                println!("Compilation status: {:?}", output.status);
-                println!(
-                    "Compilation stdout: {:?}",
-                    String::from_utf8_lossy(&output.stdout)
-                );
-                println!(
-                    "Compilation stderr: {:?}",
+                let res_text = format!(
+                    "Compilation status: {:?}\nCompilation stdout: {:?}\nCompilation stderr: {:?}",
+                    output.status,
+                    String::from_utf8_lossy(&output.stdout),
                     String::from_utf8_lossy(&output.stderr)
                 );
+                println!("{}", res_text);
+                res_tx
+                    .send(MsgToRes::new(task.id, &res_text))
+                    .await
+                    .unwrap();
 
-                run_tx.send(FileToRun::new(&exec_file)).await.unwrap();
+                run_tx
+                    .send(FileToRun::new(task.id, &exec_file))
+                    .await
+                    .unwrap();
             });
         }
     });
 
+    let run_res_tx = res_tx.clone();
     tokio::spawn(async move {
         while let Some(task) = run_rx.recv().await {
-            println!("Running file: {:?}", task);
+            let task_res_tx = run_res_tx.clone();
+            tokio::spawn(async move {
+                let res_tx = task_res_tx;
+                println!("Running file: {:?}", task);
 
-            let output = Command::new(task.file_name).output().await.unwrap();
+                let output = Command::new(task.file_name).output().await.unwrap();
 
-            println!("Execution status: {:?}", output.status);
-            println!(
-                "Execution stdout: {:?}",
-                String::from_utf8_lossy(&output.stdout)
-            );
-            println!(
-                "Execution stderr: {:?}",
-                String::from_utf8_lossy(&output.stderr)
-            );
+                let res_text = format!(
+                    "Execution status: {:?}\nExecution stdout: {:?}\nExecution stderr: {:?}",
+                    output.status,
+                    String::from_utf8_lossy(&output.stdout),
+                    String::from_utf8_lossy(&output.stderr)
+                );
+                println!("{}", res_text);
+                res_tx
+                    .send(MsgToRes::new(task.id, &res_text))
+                    .await
+                    .unwrap();
+            });
         }
     });
 
-    req_handler.await.unwrap();
+    let res_task_sockets = Arc::clone(&sockets);
+    tokio::spawn(async move {
+        while let Some(msg) = res_rx.recv().await {
+            println!("Received message: {:?}", msg);
+
+            let Some(socket) = res_task_sockets.get(&msg.id) else {
+                eprintln!("Socket not found for ID {}", msg.id);
+                continue;
+            };
+
+            let socket_clone = socket.clone();
+            let message = format!("{}\n", msg.text);
+
+            tokio::spawn(async move {
+                let result = {
+                    let mut stream = socket_clone.lock().await;
+                    let write_result = stream.write_all(message.as_bytes()).await;
+                    if let Err(e) = write_result {
+                        eprintln!("Failed to write to socket: {}", e);
+                        return;
+                    }
+                    stream.flush().await
+                };
+
+                if let Err(e) = result {
+                    eprintln!("Failed to flush socket: {}", e);
+                }
+            });
+        }
+    });
+
+    accept_task.await.unwrap();
 
     Ok(())
 }
--- src/models.rs
+++ src/models.rs
@@ -1,11 +1,43 @@
 #[derive(Debug, Clone)]
+pub struct MsgToHandle {
+    pub id: uuid::Uuid,
+    pub text: String,
+}
+
+impl MsgToHandle {
+    pub fn new(id: uuid::Uuid, text: &str) -> Self {
+        MsgToHandle {
+            id,
+            text: text.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct MsgToRes {
+    pub id: uuid::Uuid,
+    pub text: String,
+}
+
+impl MsgToRes {
+    pub fn new(id: uuid::Uuid, text: &str) -> Self {
+        MsgToRes {
+            id,
+            text: text.to_string(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
 pub struct FileToCompile {
+    pub id: uuid::Uuid,
     pub file_name: String,
 }
 
 impl FileToCompile {
-    pub fn new(file_name: &str) -> Self {
+    pub fn new(id: uuid::Uuid, file_name: &str) -> Self {
         FileToCompile {
+            id,
             file_name: file_name.to_string(),
         }
     }
@@ -13,12 +45,14 @@ impl FileToCompile {
 
 #[derive(Debug, Clone)]
 pub struct FileToRun {
+    pub id: uuid::Uuid,
     pub file_name: String,
 }
 
 impl FileToRun {
-    pub fn new(file_name: &str) -> Self {
+    pub fn new(id: uuid::Uuid, file_name: &str) -> Self {
         FileToRun {
+            id,
             file_name: file_name.to_string(),
         }
     }
