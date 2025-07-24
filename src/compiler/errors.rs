311e9f716b28476010b7f89c8bc87702ff215dc7
Artem Kudyakov <t3m8ch@yandex.ru>
2025-07-24 17:47:56 +0400

    chore: Add TODO

--- src/compiler/errors.rs
+++ src/compiler/errors.rs
@@ -1,5 +1,6 @@
 use crate::domain::CompilationLimitType;
 
+// TODO: Separate student error and server error when compiling
 #[derive(Debug, Clone)]
 pub enum CompilationError {
     CompilationFailed { msg: String },
