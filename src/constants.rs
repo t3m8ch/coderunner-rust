fae54482dd70e7ff3017ba65ebf27d9db51764a5
Artem Kudyakov <t3m8ch@yandex.ru>
2025-07-29 17:18:24 +0400

    refactor: Remove redundant 'static' lifetimes

--- src/constants.rs
+++ src/constants.rs
@@ -1,4 +1,4 @@
-pub const TASK_TX_ERR: &'static str = "Failed to send task to task_tx";
-pub const RUN_TX_ERR: &'static str = "Failed to send task to run_tx";
-pub const STREAM_TX_ERR: &'static str = "Failed to send task to stream_tx";
-pub const COMPILE_TX_ERR: &'static str = "Failed to send task to compile_tx";
+pub const TASK_TX_ERR: &str = "Failed to send task to task_tx";
+pub const RUN_TX_ERR: &str = "Failed to send task to run_tx";
+pub const STREAM_TX_ERR: &str = "Failed to send task to stream_tx";
+pub const COMPILE_TX_ERR: &str = "Failed to send task to compile_tx";
